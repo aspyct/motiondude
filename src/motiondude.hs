225736bcf0c758b0018953536e1388112c48859a
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-03-25 21:19:09 +0100

    Implemented the echo service.
    How brilliant :D

--- src/motiondude.hs
+++ src/motiondude.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Main where
 
 import Control.Applicative ((<$>), optional)
@@ -5,7 +6,7 @@ import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text.Lazy (unpack)
 import Happstack.Lite
-import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
+import Text.Blaze.Html5 (Html, (!), a, form, input, h1, p, toHtml, label)
 import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
 
 import qualified Text.Blaze.Html5 as H
@@ -16,18 +17,27 @@ main = serve Nothing myApp
 
 myApp :: ServerPart Response
 myApp = msum
-    [ echo
+    [   dir "echo" $ echo,
+        homePage
     ]
+
+homePage :: ServerPart Response
+homePage =
+    ok $ template "home page" $ do
+        h1 "Hello world"
+        p "Haskell programming is wild !"
 
 echo :: ServerPart Response
 echo =
     path $ \(msg :: String) ->
         ok $ template "echo" $ do
-          p $ "echo says: " >> toHtml msg
-          p "Change the URL to echo something else."
+            h1 "Echo service"
+            p "Giant, Haskell style Papagallo"
+            p $ toHtml msg
+
 
 template :: Text -> Html -> Response
-template title body = toResponse
+template title body = toResponse $
     H.html $ do
         H.head $ do
             H.title (toHtml title)
